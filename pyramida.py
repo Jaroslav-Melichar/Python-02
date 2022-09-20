bec548cc25241d130c3b59f699f8eb5d90cd3147
Jaroslav-Melichar <79139968+Jaroslav-Melichar@users.noreply.github.com>
2022-09-20 14:54:39 +0200

    Update pyramida.py

--- pyramida.py
+++ pyramida.py
@@ -2,8 +2,8 @@ row = int(input('number of stars: '))
 
 for i in range(row,0,-1):
     for j in range(row-i):
-        print(' ', end='') # printing space and staying in same line
+        print(' ', end='') 
     
     for j in range(2*i-1):
-        print('*',end='') # printing * and staying in same line
-    print() # printing new line
+        print('*',end='') 
+    print() 
