8d6ad3666c56c1bb81cfa83c4d1390d3abca7168
Jaroslav-Melichar <79139968+Jaroslav-Melichar@users.noreply.github.com>
2022-09-27 10:23:15 +0200

    Update nasobeni.py

--- nasobeni.py
+++ nasobeni.py
@@ -1,9 +1,9 @@
-print("zadejte prvni číslo")
+print("zadejte prvni číslo:")
 x = int(input())
-print("zadejte druhe číslo")
+print("zadejte druhe číslo:")
 y = int(input())
 print('Výsledek : ')
-if x + y <= 1000:
+if x * y <= 1000:
   print(x*y)
 else :
   print(x+y)
