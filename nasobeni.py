b81db5c9a764b9493bfed496006b03dc85cf3209
Jaroslav-Melichar <79139968+Jaroslav-Melichar@users.noreply.github.com>
2022-09-27 10:27:54 +0200

    ,

--- nasobeni.py
+++ nasobeni.py
@@ -2,7 +2,7 @@ print("zadejte prvni číslo:")
 x = int(input())
 print("zadejte druhe číslo:")
 y = int(input())
-print('Výsledek : ')
+print('Výsledek je: ')
 if x * y <= 1000:
   print(x*y)
 else :
